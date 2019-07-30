2f7bdf42924c2e1dcfd4f4d25b943e83514d70f4
zyzeng <zyzeng@ctrip.com>
2019-07-30 13:25:02 +0800

    初始化代码

--- README.md
+++ README.md
@@ -1,9 +1,79 @@
 eredis_ms
 =====
 
-An OTP application
+Redis Muti Master-Slave Client. Need ems config.
 
 Build
 -----
 
     $ rebar3 compile
+    $ erl -pa _build/default/lib/*/ebin -pa _build/default/lib/*/priv -config sys.config -s eredis_ms
+	> eredis_ms:set(pool,<<"a">>,<<"1">>).
+	{ok,<<"OK">>}
+	> eredis_ms:setnx(pool,<<"a">>,<<"1">>).
+	{ok,<<"0">>}
+	> eredis_ms:setex(pool,<<"a">>,5,<<"1">>).
+	{ok,<<"OK">>}
+	> eredis_ms:get(pool,<<"a">>).
+	{ok,undefined}
+	> eredis_ms:exists(pool,<<"a">>).
+	{ok,<<"0">>}
+	> eredis_ms:ttl(pool,<<"a">>).
+	{ok,<<"-1">>}
+	> eredis_ms:expire(pool,<<"a">>,5).
+	{ok,<<"1">>}
+	> eredis_ms:del(pool,<<"a">>,[<<"a">>,<<"b">>]).
+	{ok,<<"0">>}
+	> eredis_ms:mset(pool,<<"as">>,[<<"a">>,1,<<"b">>,2,<<"c">>,3,<<"d">>,4]).
+	{ok,<<"OK">>}
+	> eredis_ms:mget(pool,<<"as">>,[<<"a">>,<<"b">>,<<"c">>,<<"d">>]).
+	{ok,[<<"1">>,<<"2">>,<<"3">>,<<"4">>]}
+	
+	> eredis_ms:hset(pool,<<"ah">>,<<"a">>,2).
+	{ok,<<"0">>}
+	> eredis_ms:hget(pool,<<"ah">>,<<"a">>).
+	{ok,<<"2">>}
+	> eredis_ms:hexists(pool,<<"ah">>,<<"a">>).
+	{ok,<<"1">>}
+	> eredis_ms:hlen(pool,<<"ah">>).
+	{ok,<<"4">>}
+	> eredis_ms:hmset(pool,<<"ah">>,[<<"a">>,1,<<"b">>,2,<<"c">>,3,<<"d">>,4]).
+	{ok,<<"OK">>}
+	> eredis_ms:hmget(pool,<<"ah">>,[<<"a">>,<<"b">>,<<"c">>,<<"d">>]).
+	{ok,[<<"2">>,<<"2">>,<<"3">>,<<"4">>]}
+	> eredis_ms:hdel(pool,<<"ah">>,[<<"a">>,<<"b">>]).
+	{ok,<<"2">>}
+	> eredis_ms:hgetall(pool,<<"ah">>).
+	{ok,[<<"c">>,<<"3">>,<<"d">>,<<"4">>]}
+	
+	> eredis_ms:lpush(pool,<<"al">>,[<<"a">>,1,<<"b">>,2,<<"c">>,3,<<"d">>,4]).
+	{ok,<<"8">>}
+	> eredis_ms:lpushx(pool,<<"al">>,<<"a">>).
+	{ok,<<"9">>}
+	> eredis_ms:rpush(pool,<<"al">>,[<<"a">>,1,<<"b">>,2,<<"c">>,3,<<"d">>,4]).
+	{ok,<<"17">>}
+	> eredis_ms:rpushx(pool,<<"al">>,<<"a">>).
+	{ok,<<"18">>}
+	> eredis_ms:lpop(pool,<<"al">>).
+	{ok,<<"a">>}
+	> eredis_ms:rpop(pool,<<"al">>).
+	{ok,<<"a">>}
+	> eredis_ms:llen(pool,<<"al">>).
+	{ok,<<"16">>}	
+	
+	> eredis_ms:sadd(pool,<<"ass">>,[<<"a">>,<<"b">>,<<"c">>,<<"d">>,1]).
+	{ok,<<"5">>}
+	> eredis_ms:srem(pool,<<"ass">>,[<<"a">>,1]).
+	{ok,<<"2">>}
+	> eredis_ms:spop(pool,<<"ass">>).
+	{ok,<<"d">>}
+	> eredis_ms:smembers(pool,<<"ass">>).
+	{ok,[<<"b">>,<<"c">>]}
+	
+	> eredis_ms:qp(pool,<<"qqq">>,[["SET",<<"hello">>,<<"world">>],["GET",<<"hello">>]]).
+	[{ok,<<"OK">>},{ok,<<"world">>}]
+	
+	
+	
+	
+	
---
+++ src/eredis_ms.erl
@@ -0,0 +1,305 @@
+%% @author bai
+%% @doc @todo Add description to eredis_ms.
+
+
+-module(eredis_ms).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([
+	start/0,	 
+	
+	q/3,
+	q_noreply/3, 
+	qp/3,
+	
+	set/3,
+	setnx/3,
+	setex/4,
+	get/2,
+	exists/2,
+	ttl/2,
+	expire/3,
+	del/3,
+	mset/3,
+	mget/3,
+	
+	hset/4,
+	hget/3,
+	hexists/3,
+	hlen/2,
+	hmset/3,
+	hmget/3,
+	hdel/3,
+	hgetall/2,
+	
+	lpush/3,
+	lpushx/3,
+	rpush/3,
+	rpushx/3,
+	lpop/2,
+	rpop/2,
+	llen/2,
+	sadd/3,
+	srem/3,
+	spop/2,
+	smembers/2
+]).
+-define(TIMEOUT, 5000).
+
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+q(Poolname,Key,Commands)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, Commands, ?TIMEOUT);
+		R->
+			R
+	end.
+q_noreply(Poolname,Key,Commands)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q_noreply(Client, Commands);
+		R->
+			R
+	end.
+qp(Poolname,Key,Commands)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:qp(Client, Commands, ?TIMEOUT);
+		R->
+			R
+	end.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% 具体命令
+set(Poolname,Key,Value)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["SET",Key,Value], ?TIMEOUT);
+		R->
+			R
+	end.
+setnx(Poolname,Key,Value)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["SETNX",Key,Value], ?TIMEOUT);
+		R->
+			R
+	end.
+setex(Poolname,Key,Seconds,Value)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["SETEX",Key,Seconds,Value], ?TIMEOUT);
+		R->
+			R
+	end.
+get(Poolname,Key)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["GET",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+exists(Poolname,Key)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["EXISTS",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+ttl(Poolname,Key)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["TTL",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+expire(Poolname,Key,Seconds)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["EXPIRE",Key,Seconds], ?TIMEOUT);
+		R->
+			R
+	end.
+del(Poolname,Key,KeyPairs)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["DEL" | KeyPairs], ?TIMEOUT);
+		R->
+			R
+	end.
+mset(Poolname,Key,KeyValuePairs)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["MSET" | KeyValuePairs], ?TIMEOUT);
+		R->
+			R
+	end.
+mget(Poolname,Key,KeyPairs)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["MGET" | KeyPairs], ?TIMEOUT);
+		R->
+			R
+	end.
+hset(Poolname,Key,Field,Value)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["HSET",Key,Field,Value], ?TIMEOUT);
+		R->
+			R
+	end.
+hget(Poolname,Key,Field)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["HGET",Key,Field], ?TIMEOUT);
+		R->
+			R
+	end.
+hexists(Poolname,Key,Field)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["HEXISTS",Key,Field], ?TIMEOUT);
+		R->
+			R
+	end.
+hlen(Poolname,Key)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["HLEN",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+hmset(Poolname,Key,FieldValuePairs)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["HMSET",Key | FieldValuePairs], ?TIMEOUT);
+		R->
+			R
+	end.
+hmget(Poolname,Key,FieldPairs)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["HMGET",Key | FieldPairs], ?TIMEOUT);
+		R->
+			R
+	end.
+hdel(Poolname,Key,FieldPairs)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			%% 判断大小，改用游标
+
+			eredis:q(Client, ["HDEL",Key | FieldPairs], ?TIMEOUT);
+		R->
+			R
+	end.
+hgetall(Poolname,Key)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			%% 改用游标
+			
+			eredis:q(Client, ["HGETALL",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+lpush(Poolname,Key,ValuePairs)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["LPUSH",Key | ValuePairs], ?TIMEOUT);
+		R->
+			R
+	end.
+lpushx(Poolname,Key,Value)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["LPUSHX",Key,Value], ?TIMEOUT);
+		R->
+			R
+	end.
+rpush(Poolname,Key,ValuePairs)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["RPUSH",Key | ValuePairs], ?TIMEOUT);
+		R->
+			R
+	end.
+rpushx(Poolname,Key,Value)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["RPUSHX",Key,Value], ?TIMEOUT);
+		R->
+			R
+	end.
+lpop(Poolname,Key)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["LPOP",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+rpop(Poolname,Key)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["RPOP",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+llen(Poolname,Key)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["LLEN",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+sadd(Poolname,Key,ValuePairs)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["SADD",Key | ValuePairs], ?TIMEOUT);
+		R->
+			R
+	end.
+srem(Poolname,Key,ValuePairs)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["SREM",Key | ValuePairs], ?TIMEOUT);
+		R->
+			R
+	end.
+spop(Poolname,Key)->
+	case ems:get_client_write(Poolname,Key) of
+		{ok,Client}->
+			eredis:q(Client, ["SPOP",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+smembers(Poolname,Key)->
+	case ems:get_client_read(Poolname,Key) of
+		{ok,Client}->
+			%% 改成游标
+			
+			eredis:q(Client, ["SMEMBERS",Key], ?TIMEOUT);
+		R->
+			R
+	end.
+
+
+%% 启动方法
+start()->
+	%% 含连接从节点过程。
+	ok = start(?MODULE),
+	ok.
+%% 启动App
+start(App) ->
+    start_ok(App, application:start(App, permanent)).
+start_ok(_App, ok) -> ok;
+start_ok(_App, {error, {already_started, _App}}) -> ok;
+start_ok(App, {error, {not_started, Dep}}) ->
+    ok = start(Dep),
+    start(App);
+start_ok(App, {error, Reason}) ->
+    erlang:error({aps_start_failed, App, Reason}).
